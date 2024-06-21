c6a921f95f644c6ae779652eff3816ed948ccbd7
Avinash <vimalnash@gmail.com>
2024-06-22 00:35:52 +0530

    Renderapiupdate

--- src/Handlers/BackendUrls.jsx
+++ src/Handlers/BackendUrls.jsx
@@ -1,6 +1,8 @@
-const baseurl = "http://localhost:8090";
+// const baseurl = "http://localhost:8060";
+const baseurl = "https://weddingwise-em-backend.onrender.com";
 const adminurl = "api/admin";
 const userurl = "api/user";
 const vendorurl = "api/vendor";
 
 export { baseurl, adminurl, userurl, vendorurl }
+
